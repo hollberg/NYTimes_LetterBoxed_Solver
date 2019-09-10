3f334c9638982df51d2870d780498b47c3ba3fae
hollberg <mhollberg@gmail.com>
2019-09-10 13:14:10 -0400

    Update SpellingBeeCheat.py

--- SpellingBeeCheat.py
+++ SpellingBeeCheat.py
@@ -1,9 +1,19 @@
+'''
+SpellingBeeCheat.py
+By Mitch Hollberg (github.com/hollberg)
+Script to generate all valid words in the NYTimes "Spelling Bee" Puzzle
+
+Overview: Given a list of 7 letters, generate as many words as possible using only these letters (repetition allowed).
+One of the letters MUST appear in every word.
+'''
+
 import re
 import pandas as pd
 
 center_letter = 'N'
 other_letters = 'ADHRTY'+center_letter
 
+# Can find word list at: https://drive.google.com/file/d/0B9-WNydZzCHrdDVEc09CamJOZHc/view
 df_words = pd.read_csv('scrabble_words.txt', names=['words'], header=None)
 
 print(df_words.size)
